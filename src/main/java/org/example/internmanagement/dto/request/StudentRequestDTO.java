ad94de830665b3a2cb99062a78ede2aacb7b14c1
ltvhcung001 <ltv.h.cung001@gmail.com>
2026-05-13 16:36:46 +0700

    Done Student APIs

---
+++ src/main/java/org/example/internmanagement/dto/request/StudentRequestDTO.java
@@ -0,0 +1,18 @@
+package org.example.internmanagement.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentRequestDTO {
+    private Integer userId;
+    private String studentCode;
+    private String major;
+    private String className;
+    private LocalDate dateOfBirth;
+    private String address;
+}
